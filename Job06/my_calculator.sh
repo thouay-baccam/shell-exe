05272cbf3ad06188a59b543334a88bebf0725659
thouay-baccam <144665060+thouay-baccam@users.noreply.github.com>
2023-10-05 23:36:35 +0200

    Mis a jour des comment

--- Job06/my_calculator.sh
+++ Job06/my_calculator.sh
@@ -1,1 +1,4 @@
+# Cette commande 'bc' effectue des calculs arithmétiques.
+# Pour multiplier, assurez-vous d'utiliser l'opérateur '*' (étoile) avec des apostrophes ou guillemets,
+# par exemple : '2 * 3' ou "2 * 3".
 bc -l <<< $@
