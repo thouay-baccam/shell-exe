862b89521a1a01d55ca0e4d7196fec19e094a838
thouay-baccam <144665060+thouay-baccam@users.noreply.github.com>
2023-10-05 23:30:51 +0200

    Commentaire et ajout au script.

--- Job05/hello_bye.sh
+++ Job05/hello_bye.sh
@@ -1,6 +1,10 @@
+# Ce script vérifie la valeur du premier argument ($1)
+# et affiche un message en conséquence.
+
 if [[ "$1" == "Hello" ]]; then
   echo "Bonjour, je suis un script !"
 elif [[ "$1" == "Bye" ]]; then
   echo "Au revoir et bonne journée !"
+else
+  echo "Usage : $0 [Hello|Bye]"
 fi
-
