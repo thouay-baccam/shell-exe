5ac01ce5c81f4b893a4a3b9f0440a02a18dc1c12
thouay-baccam <144665060+thouay-baccam@users.noreply.github.com>
2023-10-05 23:39:01 +0200

    Commentaire MaJ

--- Job07/myloop.sh
+++ Job07/myloop.sh
@@ -1,3 +1,5 @@
+# Cette boucle 'for' itère de 1 à 10 en utilisant la commande 'seq'.
+# À chaque itération, elle affiche un message avec le numéro de l'itération.
 for i in $(seq 1 10); do
-echo "Je suis un script qui arrive faire une boucle" $i
+  echo "Je suis un script qui sait faire une boucle" $i
 done
