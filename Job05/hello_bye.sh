f9a257b45bb0b9d0fac85f3cdbc43062a4ce5644
thouay-baccam <144665060+thouay-baccam@users.noreply.github.com>
2023-10-05 23:32:06 +0200

    Update comment

--- Job05/hello_bye.sh
+++ Job05/hello_bye.sh
@@ -1,10 +1,11 @@
-# Ce script vérifie la valeur du premier argument ($1)
-# et affiche un message en conséquence.
+# Ce script vérifie la valeur du premier argument ($1) et affiche un message en conséquence.
+
+# Exemple d'utilisation :
+# Pour saluer avec "Hello", exécutez : "./nom_du_script.sh Hello"
+# Pour dire au revoir avec "Bye", exécutez : "./nom_du_script.sh Bye"
 
 if [[ "$1" == "Hello" ]]; then
   echo "Bonjour, je suis un script !"
 elif [[ "$1" == "Bye" ]]; then
   echo "Au revoir et bonne journée !"
-else
-  echo "Usage : $0 [Hello|Bye]"
 fi
