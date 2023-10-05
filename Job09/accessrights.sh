8a926727b439ca01d2aabc34abdcbfa49eae4f22
thouay-baccam <144665060+thouay-baccam@users.noreply.github.com>
2023-10-05 23:42:56 +0200

    MaJ Script & Comments

--- Job09/accessrights.sh
+++ Job09/accessrights.sh
@@ -1,28 +1,23 @@
-#!/bin/bash
-
+# Vérifie si 'Shell_Userlist.csv' a été modifié.
 path=$(dirname "$0")
 modifsave=$(cat "$path/modifsave")
 lastmodif=$(date -r "$path/Shell_Userlist.csv" '+%Y-%m-%d %H:%M:%S')
 
+# Si le fichier a été modifié, crée et attribue des utilisateurs avec des rôles.
 if [[ "$modifsave" != "$lastmodif" ]]; then
     while IFS="," read -r id prenom nom mdp role; do
         username=$(echo "$prenom.$nom" | tr '[:upper:]' '[:lower:]')
 
-        # Vérifiez la longueur du nom d'utilisateur
-        if [ ${#username} -lt 3 ]; then
-            continue  # Ignore les noms d'utilisateurs de moins de 3 caractères
-        fi
+        # Ignore les noms d'utilisateurs de moins de 3 caractères.
+        [ ${#username} -lt 3 ] && continue
 
         nrole=$(echo "$role" | tr -d '\r' | cat -t)
         sudo useradd "$username"
         echo "$username:$mdp" | sudo chpasswd
-        if [ "$nrole" = "Admin" ]; then
-            sudo usermod -aG sudo "$username"
-            echo "did $username"
-        else
-            sudo usermod -aG users "$username"
-            echo "did $username"
-        fi
+        [ "$nrole" = "Admin" ] && sudo usermod -aG sudo "$username" || sudo usermod -aG users "$username"
+        echo "did $username"
     done < <(tail -n +2 "$path/Shell_Userlist.csv" | tr -d " " && echo "")
+    
+    # Met à jour le timestamp de modification.
     date -r "$path/Shell_Userlist.csv" '+%Y-%m-%d %H:%M:%S' > "$path/modifsave"
 fi
